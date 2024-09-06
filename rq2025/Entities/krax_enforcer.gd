888a769743435bba406df9ebb79e2363beafee41
Gareth <gareth@redovoting.io>
2024-09-06 18:07:05 -0400

    flank function works pretty well

--- rq2025/Entities/krax_enforcer.gd
+++ rq2025/Entities/krax_enforcer.gd
@@ -14,13 +14,13 @@ func _physics_process(_delta):
 			state_machine(states.IDLE)
 			target_player()
 		else:
-			set_range()
+			get_distance_to_player()
 			movement_loop()
 			spritedir_loop()
 			if state == states.SEEK:
 				seek()
 			elif state == states.ENGAGE:
-				movedir = Vector2()
+				engage()
 
 func target_player():
 	var player_tracker = level_manager.player_tracker
@@ -54,13 +54,48 @@ func set_role(pt):
 	level_manager.update_assigned_enemies(targeted_player_id, self.get_instance_id(), role)
 
 func seek():
-	var player_node = level_manager.player_tracker[targeted_player_id].node
-	movedir = global_position.direction_to(player_node.global_position)
+	var player_position = get_targeted_player_position()
+	movedir = global_position.direction_to(player_position)
 
-func set_range():
-	var player_node = level_manager.player_tracker[targeted_player_id].node
-	var distance_from_player = global_position.distance_to(player_node.global_position)
+func engage():
+	if role == roles.FLANKER:
+		flank()
+	else:
+		get_on_line()
+	
+func get_distance_to_player():
+	var player_position = get_targeted_player_position()
+	var distance_from_player = global_position.distance_to(player_position)
 	if distance_from_player > 300:
 		state_machine(states.SEEK)
 	elif distance_from_player > 100:
 		state_machine(states.ENGAGE)
+
+func get_targeted_player_position():
+	return level_manager.player_tracker[targeted_player_id].node.global_position
+	
+func get_on_line():
+	var player_position = get_targeted_player_position()
+	if abs(player_position.y) - abs(global_position.y) > 100:
+		if player_position.y < global_position.y:
+			movedir = Vector2(-1, 0)
+		elif player_position.y > global_position.y:
+			movedir = Vector2(1, 0)
+
+func flank():
+	# Get player's position
+	var player_position = get_targeted_player_position()
+
+	# Calculate the direction from the enemy to the player
+	var direction_to_player = global_position.direction_to(player_position)
+
+	if player_position.y < global_position.y:
+		# If the player is below the enemy, flank above
+		# Move the enemy counterclockwise in the hemisphere above the player
+		movedir = Vector2(-direction_to_player.y, direction_to_player.x).normalized()
+	else:
+		# If the player is above the enemy, flank below
+		# Move the enemy clockwise in the hemisphere below the player
+		movedir = Vector2(direction_to_player.y, -direction_to_player.x).normalized()
+
+	# Move the enemy in the chosen direction while maintaining the desired radius
