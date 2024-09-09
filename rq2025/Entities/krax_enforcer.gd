ff2a0e503c1e82d71e55e7327d310e4b3c8e8e62
Gareth <gareth@redovoting.io>
2024-09-09 16:40:26 -0400

    created orthogonal coefficient

--- rq2025/Entities/krax_enforcer.gd
+++ rq2025/Entities/krax_enforcer.gd
@@ -36,6 +36,7 @@ func target_player():
 	var least_agro_players = level_manager.get_least_agro_players()
 	get_closest_player(least_agro_players)
 	
+	
 func get_closest_player(player_ids):
 	var closest_player = null
 	var closest_distance = INF  # Start with a large number to compare distances
@@ -66,6 +67,7 @@ func seek():
 	movedir = global_position.direction_to(get_targeted_player_position())
 	
 func engage():
+	get_orthogonal_coefficient()
 	if role == roles.AGGRESSOR:
 		aggress()
 	elif role == roles.FLANKER:
@@ -75,7 +77,7 @@ func engage():
 		
 func flank():
 	var direction_to_player = get_direction_to_player()
-	var perpendicular_vector = direction_to_player.orthogonal()
+	var perpendicular_vector = direction_to_player.orthogonal() * get_orthogonal_coefficient()
 	movedir = perpendicular_vector.normalized()
 
 	if not aggressor_is_same_side() and get_is_on_line():
@@ -118,6 +120,17 @@ func get_is_on_line():
 	
 func set_is_on_line():
 	getting_on_line = abs(global_position.y - get_targeted_player_position().y) == 0
+	
+func get_orthogonal_coefficient():
+	var direction_to_player = get_direction_to_player()
+	var below_player = direction_to_player.y <= 0
+	var above_player = direction_to_player.y > 0
+	var left_of_player = direction_to_player.x > 0
+	var right_of_player = direction_to_player.x <= 0
+	if below_player and left_of_player or above_player and right_of_player:
+		return -1
+	if below_player and right_of_player or above_player and left_of_player:
+		return 1
 
 func adjust_distance():
 	var player_position = get_targeted_player_position()
